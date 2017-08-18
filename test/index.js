0251e01120dacdc16127361b67cf18c17899beee
rusty1s <matthias.fey@tu-dortmund.de>
2017-08-18 09:13:45 +0200

    gh pages begin

--- test/index.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-import './spec/setup';
-import './spec/init';
-import './spec/methods';
-import './spec/events/click';
-import './spec/events/keydown';
-import './spec/events/dispatch';
